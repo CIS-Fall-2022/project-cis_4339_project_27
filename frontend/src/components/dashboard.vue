1c7c7c012af692e7ba4ae602e9b9a617d17f0f62
mmali9 <aliance467@gmail.com>
2022-10-29 13:54:28 -0500

    add event functional

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -2,15 +2,59 @@
   <main>
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
+      <canvas id="myChart" width="400" height="400"></canvas>
     </div>
   </main>
 </template>
 <script>
+import Chart from 'chart.js/auto';
+
 export default {
   methods: {
     routePush(routeName) {
       this.$router.push({ name: routeName });
     },
   },
+  mounted() {
+  console.log('Component mounted.')
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+  }
 };
 </script>
